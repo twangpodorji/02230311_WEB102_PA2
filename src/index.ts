def3482034b07dcbc71475ddeb0035290b65c1e9
douglasswm <douglas.cst@rub.edu.bt>
2024-06-08 18:35:36 +0600

    v1.0: rest api w/o auth guards

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { PrismaClient } from "@prisma/client";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const prisma = new PrismaClient();
 
-export default app
+app.use("/*", cors());
+
+app.get("/:userId/account/balance", async (c) => {
+  // get user account balance from url params
+  const { userId } = c.req.param();
+
+  // create a new user
+  const user = await prisma.user.findUnique({
+    where: { id: userId },
+    select: { Account: { select: { balance: true, id: true } } },
+  });
+  return c.json({ data: user });
+});
+
+export default app;
