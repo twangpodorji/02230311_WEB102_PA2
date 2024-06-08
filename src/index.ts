406bc2a7f2dad058cb6e7a41dbdc2352f636e750
douglasswm <douglas.cst@rub.edu.bt>
2024-06-08 20:51:36 +0600

    version: jwt email/pw auth

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,38 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Prisma } from "@prisma/client";
+import { HTTPException } from "hono/http-exception";
+import { decode, sign, verify } from "hono/jwt";
+import { jwt } from 'hono/jwt'
+import type { JwtVariables } from 'hono/jwt'
 
-const app = new Hono();
+type Variables = JwtVariables
+
+const app = new Hono<{ Variables: Variables }>()
 
 const prisma = new PrismaClient();
 
 app.use("/*", cors());
+
+app.use(
+  "/protected/*",
+  jwt({
+    secret: 'mySecretKey',
+  })
+);
+
+app.get("/protected/account/balance", async (c) => {
+  const payload = c.get('jwtPayload')
+  if (!payload) {
+    throw new HTTPException(401, { message: "Unauthorized" });
+  }
+  const user = await prisma.user.findUnique({
+    where: { id: payload.sub },
+    select: { Account: { select: { balance: true, id: true } } },
+  });
+
+  return c.json({ data: user });
+});
 
 app.get("/:userId/account/balance", async (c) => {
   // get user account balance from url params
@@ -18,6 +44,74 @@ app.get("/:userId/account/balance", async (c) => {
     select: { Account: { select: { balance: true, id: true } } },
   });
   return c.json({ data: user });
+});
+
+app.post("/register", async (c) => {
+  try {
+    const body = await c.req.json();
+
+    const bcryptHash = await Bun.password.hash(body.password, {
+      algorithm: "bcrypt",
+      cost: 4, // number between 4-31
+    });
+
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        hashedPassword: bcryptHash,
+        Account: {
+          create: {
+            balance: 0,
+          },
+        },
+      },
+    });
+
+    return c.json({ message: `${user.email} created successfully}` });
+  } catch (e) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      // The .code property can be accessed in a type-safe manner
+      if (e.code === "P2002") {
+        console.log(
+          "There is a unique constraint violation, a new user cannot be created with this email"
+        );
+        return c.json({ message: "Email already exists" });
+      }
+    }
+  }
+});
+
+app.post("/login", async (c) => {
+  try {
+    const body = await c.req.json();
+    const user = await prisma.user.findUnique({
+      where: { email: body.email },
+      select: { id: true, hashedPassword: true },
+    });
+
+    if (!user) {
+      return c.json({ message: "User not found" });
+    }
+
+    const match = await Bun.password.verify(
+      body.password,
+      user.hashedPassword,
+      "bcrypt"
+    );
+    if (match) {
+      const payload = {
+        sub: user.id,
+        exp: Math.floor(Date.now() / 1000) + 60 * 60, // Token expires in 60 minutes
+      };
+      const secret = "mySecretKey";
+      const token = await sign(payload, secret);
+      return c.json({ message: "Login successful", token: token });
+    } else {
+      throw new HTTPException(401, { message: "Invalid credentials" });
+    }
+  } catch (error) {
+    throw new HTTPException(401, { message: "Invalid credentials" });
+  }
 });
 
 export default app;
